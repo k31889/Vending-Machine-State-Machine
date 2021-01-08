166c492f320500209a629431537da761d19aa50f
S W <WoodenHam@hotmail.com>
2021-01-08 10:59:39 +0000

    Add project files.

---
+++ Vending Machine State Machine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vending_Machine_State_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
