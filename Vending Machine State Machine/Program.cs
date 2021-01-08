badd7bfc019f582b1becdaee92057a8aea28b58d
S W <WoodenHam@hotmail.com>
2021-01-08 11:21:06 +0000

    tested and working

--- Vending Machine State Machine/Program.cs
+++ Vending Machine State Machine/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Data;
 
-namespace Vending_Machine_State_Machine
+namespace BinaryNumberValidator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StateMachine s = new StateMachine();
+
+
+            // sequence 1 - 101
+
+            s.input(1);
+            s.input(0);
+            s.input(1);
+            Console.WriteLine("Sequence 1 \nSequence Accepted ={0}\n", s.inAccept());
+
+            // sequence 2 - 000
+
+            s.reset();
+            s.input(0);
+            s.input(0);
+            s.input(0);
+            Console.WriteLine("Sequence 2 \nSequence Accepted ={0}\n", s.inAccept());
+
+            // sequence 3 - 010001101
+
+            s.reset();
+            s.input(0);
+            s.input(1);
+            s.input(0);
+            s.input(0);
+            s.input(0);
+            s.input(1);
+            s.input(1);
+            s.input(0);
+            s.input(1);
+            Console.WriteLine("Sequence 3 \nSequence Accepted ={0}\n", s.inAccept());
+
+            // sequence 4 - 0100011011
+
+            s.reset();
+            s.input(0);
+            s.input(1);
+            s.input(0);
+            s.input(0);
+            s.input(0);
+            s.input(1);
+            s.input(1);
+            s.input(0);
+            s.input(1);
+            s.input(1);
+            Console.WriteLine("Sequence 4 \nSequence Accepted ={0}\n", s.inAccept());
+
+
+
         }
     }
 }
---
+++ Vending Machine State Machine/State Machine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryNumberValidator
+{
+    class StateMachine
+    {
+
+        const int S1 = 0;   //no money entered
+        const int S2 = 1;   //5p 
+        const int S3 = 2;   //10p
+        const int S4 = 3;   //15p
+        const int S5 = 4;   //20p
+        const int S6 = 4;   //25p
+        const int S7 = 4;   //30p
+        const int S8 = 4;   //35p
+        const int S9 = 4;   //40p+
+
+        private int[,] stateMachine;
+        private int state;
+        private int acceptState;
+
+        public StateMachine()
+        {
+
+            stateMachine = new int[,] { { S2, S5 }, { S3, S6 }, { S4, S7 }, { S5, S8 }, { S6, S9 }, { S7, S9 }, { S8, S9 }, { S9, S9 }, { S9, S9 } };
+            reset();
+        }
+
+        public void input(int message)
+        {
+            state = stateMachine[state, message];
+        }
+
+        public bool inAccept() { return acceptState == state; }
+
+        public void reset()
+        {
+            state = S1;
+            acceptState = S9;
+        }
+    }
+}
